1bc3b1a0cc34d8d6bbec303c047c62099f3f4e86
Merlin Rock <rock.merlin@yahoo.com>
2020-12-02 18:02:39 -0500

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+// var timer = setInterval(myTimer, 1000)
+
+// var questionsec = document.querySelector("#questionSection")
+
+// let arrayQuestions = [
+
+
+
+
+
+
+
+
+
+
+
+
+// ]
+
+// function.promt() {
+//     // Go through a collection of questions
+//     var question = document.getElementById("question");
+//     var answer1 = document.getElementById("answer1");
+//     var answer2 = document.getElementById("answer2");
+//     var answer3 = document.getElementById("answer3");
+
+//     question.innerHTML = 
+
+
+// }
+
+
+function startQuiz() {
+    var startBtn = document.getElementById("questionSection");
+    if (startBtn.style.display == "none") {
+        startBtn.style.display = "block";
+    }
+    else {
+        startBtn.style.display = "block";
+    }
+}
